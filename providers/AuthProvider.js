d2737bc4db996c1e06ab4a5b479b89e9cc84807e
Ricky <ricky@SaaShas-MacBook-Pro-3.local>
2022-12-07 22:44:23 -0500

    prettier & added id to login payload

--- providers/AuthProvider.js
+++ providers/AuthProvider.js
@@ -1,185 +1,177 @@
-import React from 'react';
-import { Platform } from 'react-native';
-import * as SecureStore from 'expo-secure-store';
+import React from "react";
+import { Platform } from "react-native";
+import * as SecureStore from "expo-secure-store";
 
 const AuthContext = React.createContext();
 
 const AuthProvider = (props) => {
-
   const [state, dispatch] = React.useReducer(
     (prevState, action) => {
       switch (action.type) {
-        case 'RESTORE_TOKEN':
+        case "RESTORE_TOKEN":
           return {
             ...prevState,
             token: action.token,
             isLoading: false,
           };
-        case 'LOGIN_POST':
+        case "LOGIN_POST":
           return {
             ...prevState,
             isLoading: true,
-          }
-  
-        ;
-        case 'LOGIN_SUCCESS':
+          };
+        case "LOGIN_SUCCESS":
           return {
             ...prevState,
+            id: action.id,
             isLoading: false,
             isLoggedIn: true,
             token: action.token,
             username: action.username,
             email: action.email,
             loggingIn: false,
-            error: {}
+            error: {},
           };
-  
-        case 'LOGIN_FAIL':
+
+        case "LOGIN_FAIL":
           return {
             ...prevState,
             isLoading: false,
             loggingIn: false,
             error: {
-              type: 'login',
+              type: "login",
               message: action.error,
-            }
-          }
-          ;
-        case 'LOGOUT':
+            },
+          };
+        case "LOGOUT":
           return {
             ...prevState,
             token: null,
             isLoggedIn: false,
           };
-        case 'REGISTER_POST':
+        case "REGISTER_POST":
           return {
             ...prevState,
-            isLoading: true
-          }
-        case 'REGISTER_SUCCESS':
-
-            return {
-              ...prevState,
-              isLoading: false,
-              loggingIn: true
-            }
-        case 'REGISTER_FAIL':
-            return {
-                ...prevState,
-                isLoading: false,
-                error: {
-                  type: 'register',
-                  message: action.error,
-                }
-            }
+            isLoading: true,
+          };
+        case "REGISTER_SUCCESS":
+          return {
+            ...prevState,
+            isLoading: false,
+            loggingIn: true,
+          };
+        case "REGISTER_FAIL":
+          return {
+            ...prevState,
+            isLoading: false,
+            error: {
+              type: "register",
+              message: action.error,
+            },
+          };
       }
     },
     {
+      id: null,
       username: null,
       email: null,
       token: null,
       isLoading: false,
       isLoggedIn: false,
       loggingIn: false,
-      error: {}
+      error: {},
     }
   );
-  
-  
+
   const authContext = React.useMemo(
-      () => ({
-        signIn: async (credentials) => {
-          const {username, password} = credentials;
-          const lcun = username.toLowerCase();
-          dispatch({
-              type: 'LOGIN_POST',
-            });
-              await fetch('http://10.0.0.109:3005/user/login', {
-              method:'POST',
-              headers: {
-                 'Accept': 'application/json',
-                 'Content-Type':'application/json',
-              },
-              body: JSON.stringify({
-                  username: lcun,
-                  password
-              })
-              })
-              .then(async res => {
-                const data = await res.json();
-                if(res.status === 200) {
-                  dispatch({
-                    type: 'LOGIN_SUCCESS',
-                    ...data
-                  });
-                  if(Platform.OS === 'web') {
-                    localStorage.setItem("token", data.token);
-                  } else {
-                      await SecureStore.setItemAsync("token", data.token);
-                  }
-                } else{
-                  dispatch({
-                    type: 'LOGIN_FAIL',
-                    ...data
-                  })
-                }
-              })
-              .catch(error => {
-                dispatch({
-                  type: 'LOGIN_FAIL',
-                  payload: error,
-                });
+    () => ({
+      signIn: async (credentials) => {
+        const { username, password } = credentials;
+        const lcun = username.toLowerCase();
+        dispatch({
+          type: "LOGIN_POST",
+        });
+        await fetch("http://10.0.0.165:3005/user/login", {
+          method: "POST",
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            username: lcun,
+            password,
+          }),
+        })
+          .then(async (res) => {
+            const data = await res.json();
+            if (res.status === 200) {
+              dispatch({
+                type: "LOGIN_SUCCESS",
+                ...data,
               });
-        },
-        signOut: async () => {
-          dispatch({type:"LOGOUT"});
-          if(Platform.OS === 'web') {
-            localStorage.removeItem("token");
-          } else {
-              await SecureStore.deleteItemAsync("token");
-          }
-        },
-        register: async (credentials) => {
-          const {email, username, password} = credentials;
-          dispatch({type: 'REGISTER_POST'});
-          const result = await fetch('http://10.0.0.109:3005/user/register', {
-            method:'POST',
-            headers: {
-               'Accept': 'application/json',
-               'Content-Type':'application/json',
-            },
-            body: JSON.stringify({
-                email,
-                username,
-                password
-            })
-            })
-            .then(async res => {
-              const data = await res.json();
-              if(res.status === 201) {
-                dispatch({type: "REGISTER_SUCCESS"});
+              if (Platform.OS === "web") {
+                localStorage.setItem("token", data.token);
               } else {
-                dispatch({type: "REGISTER_FAIL", ...data});
+                await SecureStore.setItemAsync("token", data.token);
               }
-            })
-            .catch(error => {
-              dispatch({type: "REGISTER_FAIL", payload: error});
-            })
-            return result;
+            } else {
+              dispatch({
+                type: "LOGIN_FAIL",
+                ...data,
+              });
+            }
+          })
+          .catch((error) => {
+            dispatch({
+              type: "LOGIN_FAIL",
+              payload: error,
+            });
+          });
+      },
+      signOut: async () => {
+        dispatch({ type: "LOGOUT" });
+        if (Platform.OS === "web") {
+          localStorage.removeItem("token");
+        } else {
+          await SecureStore.deleteItemAsync("token");
         }
-      }),
-      []
+      },
+      register: async (credentials) => {
+        const { email, username, password } = credentials;
+        dispatch({ type: "REGISTER_POST" });
+        const result = await fetch("http://10.0.0.165:3005/user/register", {
+          method: "POST",
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            email,
+            username,
+            password,
+          }),
+        })
+          .then(async (res) => {
+            const data = await res.json();
+            if (res.status === 201) {
+              dispatch({ type: "REGISTER_SUCCESS" });
+            } else {
+              dispatch({ type: "REGISTER_FAIL", ...data });
+            }
+          })
+          .catch((error) => {
+            dispatch({ type: "REGISTER_FAIL", payload: error });
+          });
+        return result;
+      },
+    }),
+    []
   );
 
-  return (
-    <AuthContext.Provider value={{state, authContext}}  {...props} />
-  );
-}
+  return <AuthContext.Provider value={{ state, authContext }} {...props} />;
+};
 
 const useAuthContext = () => {
   return React.useContext(AuthContext);
-}
+};
 
 export { AuthProvider, useAuthContext };
-
-
