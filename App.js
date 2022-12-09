ad758ebcc2035a455bc6d93681fe037f59493260
Ricky <ricky@SaaShas-MacBook-Pro-3.local>
2022-12-08 20:33:39 -0500

    wrapped in project provider

--- App.js
+++ App.js
@@ -1,14 +1,17 @@
-import 'react-native-gesture-handler';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { AuthProvider } from './providers/AuthProvider';
-import Navigator from './components/Navigator';
+import "react-native-gesture-handler";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { AuthProvider } from "./providers/AuthProvider";
+import { ProjectProvider } from "./providers/ProjectProvider";
+import Navigator from "./components/Navigator";
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <AuthProvider>
+      <ProjectProvider>
         <Navigator />
+      </ProjectProvider>
     </AuthProvider>
   );
 }
