39e1c0bf66f7afbfafd4abf4ce976f6dfca3a11a
Ricky <ricky@SaaShas-MacBook-Pro-3.local>
2022-12-08 20:37:08 -0500

    WIP include user projects on dashboard

--- views/Dashboard.js
+++ views/Dashboard.js
@@ -1,48 +1,104 @@
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import React, { useState, useEffect } from "react";
+import {
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+  ActivityIndicator,
+} from "react-native";
+import { useAuthContext } from "../providers/AuthProvider";
+import { useProjectContext } from "../providers/ProjectProvider";
 
-const Dashboard = ({navigation}) => {
+const Dashboard = ({ navigation }) => {
+  const [ready, setReady] = useState(false);
+  const { state: authState } = useAuthContext();
+  const { state: projectState, projectContext } = useProjectContext();
+  const { getProjectsByUser } = projectContext;
+
+  const { id } = authState;
+  const { projects } = projectState;
+
+  useEffect(() => {
+    if (!ready) {
+      Promise.all(getProjectsByUser(id)).then(() => {
+        setReady(true);
+      });
+    }
+  }, [ready]);
+
+  console.log(projects.length);
+
   return (
     <View style={styles.container}>
-      <View style={{
-        backgroundColor:'white',
-        marginTop: 8,
-      }}>
-        <View style={{
-          padding: 8
-        }}>
-          <Text>
-            My Workbench
-          </Text>
+      <View
+        style={{
+          backgroundColor: "white",
+          marginTop: 8,
+          minHeight: 200,
+        }}
+      >
+        <View
+          style={{
+            padding: 8,
+          }}
+        >
+          <Text>My Workbench</Text>
         </View>
-        <View className='project-list' style={{
-          borderTopWidth: 1,
-          borderBottomWidth: 1,
-          borderColor: 'lightgrey' ,
-          padding: 8,
-          minHeight: 100
-        }}>
-          <Text>
-            You have no projects
-          </Text>
+        <View
+          className="project-list"
+          style={{
+            borderTopWidth: 1,
+            borderBottomWidth: 1,
+            borderColor: "lightgrey",
+            padding: 8,
+            minHeight: 120,
+            justifyContent: !ready ? "center" : null,
+          }}
+        >
+          {!ready ? (
+            <ActivityIndicator />
+          ) : (
+            <View>
+              {projects.length > 0 ? (
+                projects.map((proj) => {
+                  return (
+                    <View key={proj._id}>
+                      <TouchableOpacity onPress={() => console.log(proj)}>
+                        <Text>{proj.title}</Text>
+                      </TouchableOpacity>
+                    </View>
+                  );
+                })
+              ) : (
+                <Text>You have no projects</Text>
+              )}
+            </View>
+          )}
         </View>
-        <View style={{
-          padding: 8,
-        }}>
-          <TouchableOpacity style={{alignItems:'center'}} onPress={() => navigation.navigate("Projects", {screen:'ProjectIP'})}>
-            <Text>
-              Start A New Project
-            </Text>
+        <View
+          style={{
+            padding: 8,
+          }}
+        >
+          <TouchableOpacity
+            disabled={!ready}
+            style={{ alignItems: "center" }}
+            onPress={() =>
+              navigation.navigate("Projects", { screen: "ProjectIP" })
+            }
+          >
+            <Text>Start A New Project</Text>
           </TouchableOpacity>
         </View>
       </View>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor:'darkgrey',
+    backgroundColor: "darkgrey",
   },
 });
 
