7c1595a628606b8b31202b00312e0360016f4cf9
Ricky <ricky@SaaShas-MacBook-Pro-3.local>
2022-12-08 20:36:14 -0500

    WIP setup project provider and context

--- providers/ProjectProvider.js
+++ providers/ProjectProvider.js
@@ -1,4 +1,6 @@
-import React, { createContext, useReducer } from "react";
+import React, { createContext, useReducer, useMemo, useContext } from "react";
+
+const ProjectContext = createContext();
 
 const ProjectProvider = (props) => {
   const [state, dispatch] = useReducer(
@@ -19,10 +21,67 @@ const ProjectProvider = (props) => {
             ...prevState,
             loading: false,
           };
+        case "FIND_USER_PROJECTS_GET":
+          return {
+            ...prevState,
+            loading: true,
+          };
+        case "FIND_USER_PROJECTS_SUCCESS":
+          return {
+            ...prevState,
+            projects: action.projects,
+            loading: false,
+          };
+        case "FIND_USER_PROJECTS_FAIL":
+          return {
+            ...prevState,
+            loading: false,
+          };
       }
     },
     {
       loading: false,
+      projects: [],
     }
   );
+
+  const projectContext = useMemo(
+    () => ({
+      getProjectsByUser: async (userId) => {
+        dispatch({
+          type: "FIND_USER_PROJECTS_GET",
+        });
+        await fetch(`http://10.0.0.165:3005/projects/${userId}`, {
+          method: "GET",
+          headers: {
+            Accept: "application/json",
+          },
+        })
+          .then(async (res) => {
+            const data = await res.json();
+            if (res.status === 200) {
+              dispatch({
+                type: "FIND_USER_PROJECTS_SUCCESS",
+                ...data,
+              });
+            }
+          })
+          .catch((error) => {
+            dispatch({ type: "FIND_USER_PROJECTS_FAIL" });
+            console.log(error);
+          });
+      },
+    }),
+    []
+  );
+
+  return (
+    <ProjectContext.Provider value={{ state, projectContext }} {...props} />
+  );
 };
+
+const useProjectContext = () => {
+  return useContext(ProjectContext);
+};
+
+export { ProjectProvider, useProjectContext };
