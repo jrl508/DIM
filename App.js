d5e147b6243870b3b9f7017518da2516dba53a9b
Jean Lafontant <jrl9298@gmail.com>
2022-12-12 22:33:29 -0500

    rm unused code

--- App.js
+++ App.js
@@ -1,10 +1,7 @@
 import "react-native-gesture-handler";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { AuthProvider } from "./providers/AuthProvider";
 import { ProjectProvider } from "./providers/ProjectProvider";
 import Navigator from "./components/Navigator";
-
-const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
